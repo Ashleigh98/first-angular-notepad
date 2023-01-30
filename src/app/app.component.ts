3713956bc91b3b568c3bfbc1b67ba90b18a0c0ea
Ashleigh Hart <ashleigh130298hart@gmail.com>
2023-01-30 09:47:15 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],  
 })
-export class AppComponent {
-  title = 'notepad';
+
+export class AppComponent { 
+  displayVal: Array<string> = [];
+  
+  getValue(val: string){  
+    //check if value is already in the array and alert user
+    if(this.displayVal.includes(val)){
+      alert('This item is already in your notepad');
+      (document.getElementById('memo') as HTMLInputElement).value = ''
+    }else{
+      //push value in to array and clear the value input
+      this.displayVal.push(val);
+      (document.getElementById('memo') as HTMLInputElement).value = ''
+    }
+  }
+  
+  deleteItem(item:string){
+    this.displayVal = this.displayVal.filter(e=> e !== item) //filter out the item clicked on
+  }
 }
